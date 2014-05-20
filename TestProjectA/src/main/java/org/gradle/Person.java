9703260ea846c591fc89754f90ebaa27f864eea5
Levente Molnar <molnar.levente88@gmail.com>
2014-05-20 22:46:10 +0200

    cleaning gradle build files

--- TestProjectA/src/main/java/org/gradle/Person.java
+++ TestProjectA/src/main/java/org/gradle/Person.java
@@ -1,16 +1,13 @@
 package org.gradle;
 
-import org.apache.commons.collections.list.GrowthList;
-
 public class Person {
-    private final String name;
+	private final String name;
 
-    public Person(String name) {
-        this.name = name;
-        new GrowthList();
-    }
+	public Person(String name) {
+		this.name = name;
+	}
 
-    public String getName() {
-        return name;
-    }
+	public String getName() {
+		return name;
+	}
 }
