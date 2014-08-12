a587c76bfa977a1d0d4393560804e2c2301a9002
Levente Molnar <molnar.levente88@gmail.com>
2014-08-12 21:09:33 +0200

    use relative http path in test project

--- TestProjectB/src/main/webapp/hello.js
+++ TestProjectB/src/main/webapp/hello.js
@@ -1,5 +1,5 @@
 function Hello($scope, $http) {
-    $http.get('http://localhost:8080/greeting').
+    $http.get('/greeting').
         success(function(data) {
             $scope.greeting = data;
         });
