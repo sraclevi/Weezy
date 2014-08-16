d7aea817b8defcc1eee32658726b05db2b239bc2
Levente Molnar <molnar.levente88@gmail.com>
2014-08-16 18:13:26 +0200

    Add missing integration and functional tests

---
+++ Weezy/src/test/java/com/weezy/config/CoreDomainIntegrationTest.java
@@ -0,0 +1,42 @@
+package com.weezy.config;
+
+import static junit.framework.TestCase.assertEquals;
+
+import java.util.UUID;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.weezy.core.events.AllExpensesEvent;
+import com.weezy.core.events.CreateExpenseEvent;
+import com.weezy.core.events.ExpenseDetails;
+import com.weezy.core.events.RequestAllExpensesEvent;
+import com.weezy.core.services.ExpenseService;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = { CoreConfig.class })
+public class CoreDomainIntegrationTest {
+
+	@Autowired
+	ExpenseService	expenseService;
+
+	// TODOCUMENT We have already asserted the correctness of the collaboration.
+	// This is to check that the wiring in CoreConfig works.
+	// We do this by inference.
+	@Test
+	public void addANewExpenseToTheSystem() {
+
+		CreateExpenseEvent ev = new CreateExpenseEvent(new ExpenseDetails(
+				UUID.randomUUID()));
+
+		expenseService.createExpense(ev);
+
+		AllExpensesEvent allExpenses = expenseService
+				.requestAllExpenses(new RequestAllExpensesEvent());
+
+		assertEquals(1, allExpenses.getExpensesDetails().size());
+	}
+}
---
+++ Weezy/src/test/java/com/weezy/rest/functional/ExpenseFunctionalTests.java
@@ -0,0 +1,58 @@
+package com.weezy.rest.functional;
+
+import static com.weezy.rest.controller.fixture.RestDataFixture.YUMMY_ITEM;
+import static junit.framework.TestCase.assertEquals;
+import static junit.framework.TestCase.assertTrue;
+
+import java.util.Arrays;
+
+import org.junit.Ignore;
+import org.junit.Test;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+import com.weezy.rest.controller.fixture.RestDataFixture;
+import com.weezy.rest.domain.Expense;
+
+@Ignore
+public class ExpenseFunctionalTests {
+
+	@Test
+	public void thatExpensesCanBeAddedAndQueried() {
+
+		ResponseEntity<Expense> entity = createExpense();
+
+		String path = entity.getHeaders().getLocation().getPath();
+
+		assertEquals(HttpStatus.CREATED, entity.getStatusCode());
+		assertTrue(path.startsWith("/expenses/"));
+		Expense expense = entity.getBody();
+
+		System.out.println("The Expense ID is " + expense.getKey());
+		System.out.println("The Location is "
+				+ entity.getHeaders().getLocation());
+
+		assertEquals(YUMMY_ITEM, expense.getName());
+	}
+
+	private ResponseEntity<Expense> createExpense() {
+		HttpEntity<String> requestEntity = new HttpEntity<String>(
+				RestDataFixture.standardExpenseJSON(), getHeaders());
+
+		RestTemplate template = new RestTemplate();
+		return template.postForEntity("http://localhost:8080/expenses",
+				requestEntity, Expense.class);
+	}
+
+	static HttpHeaders getHeaders() {
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_JSON);
+		headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+
+		return headers;
+	}
+}
