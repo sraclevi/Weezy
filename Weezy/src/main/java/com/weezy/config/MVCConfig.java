4660ace1592fc5a060280007c64051888759131a
Levente Molnar <molnar.levente88@gmail.com>
2014-08-24 20:44:55 +0200

    AutoConfig in MVCConfig to be able to fetch html pages and make unit tests complete

--- Weezy/src/main/java/com/weezy/config/MVCConfig.java
+++ Weezy/src/main/java/com/weezy/config/MVCConfig.java
@@ -1,10 +1,12 @@
 package com.weezy.config;
 
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 // @EnableWebMvc
+@EnableAutoConfiguration
 @ComponentScan(basePackages = { "com.weezy.rest.controller" })
 public class MVCConfig {
 }
